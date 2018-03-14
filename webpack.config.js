17d54f44e72d0a89dba948bdb8f46edd3f819c45
Yuehan Ren <Yuehan@Yuehan-Rens-MacBook-Pro-2.local>
2018-03-14 11:59:50 -0400

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -20,15 +20,14 @@ module.exports = {
 
     module: {
         // preLoaders: [
-        //     // 报错 ？？？？？
         //     {test: /\.(js|jsx)$/, loader: "eslint-loader", exclude: /node_modules/}
         // ],
         loaders: [
             { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel' },
             { test: /\.less$/, exclude: /node_modules/, loader: 'style!css!postcss!less' },
             { test: /\.css$/, exclude: /node_modules/, loader: 'style!css!postcss' },
-            { test:/\.(png|gif|jpg|jpeg|bmp)$/i, loader:'url-loader?limit=5000' },  // 限制大小5kb
-            { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000'} // 限制大小小于5k
+            { test:/\.(png|gif|jpg|jpeg|bmp)$/i, loader:'url-loader?limit=5000' },  // the size limit to 5k
+            { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000'} //
         ]
     },
 
@@ -37,24 +36,24 @@ module.exports = {
     },
 
     postcss: [
-        require('autoprefixer') //调用autoprefixer插件，例如 display: flex
+        require('autoprefixer') //autoprefixer plugins
     ],
 
     plugins: [
-        // html 模板插件
+        // insert bundle.js into HTML
         new HtmlWebpackPlugin({
             template: __dirname + '/app/index.tmpl.html'
         }),
 
-        // 热加载插件
+        // Hot reloading
         new webpack.HotModuleReplacementPlugin(),
 
-        // 打开浏览器
+        // open browser, and run at localhost:8080
         new OpenBrowserPlugin({
           url: 'http://localhost:8080'
         }),
 
-        // 可在业务 js 代码中使用 __DEV__ 判断是否是dev模式（dev模式下可以提示错误、测试报告等, production模式不提示）
+        // when developing, using __DEV_ to make sure whether under development or not.
         new webpack.DefinePlugin({
           __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
         })
@@ -62,17 +61,15 @@ module.exports = {
 
     devServer: {
         proxy: {
-          // 凡是 `/api` 开头的 http 请求，都会被代理到 localhost:3000 上，由 koa 提供 mock 数据。
-          // koa 代码在 ./mock 目录中，启动命令为 npm run mock
           '/api': {
             target: 'http://localhost:3000',
             secure: false
           }
         },
-        contentBase: "./public", //本地服务器所加载的页面所在的目录
-        colors: true, //终端中输出结果为彩色
-        historyApiFallback: true, //不跳转
-        inline: true, //实时刷新
-        hot: true  // 使用热加载插件 HotModuleReplacementPlugin
+        contentBase: "./public",
+        colors: true,
+        historyApiFallback: true,
+        inline: true,
+        hot: true
     }
 }
